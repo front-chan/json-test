9863a759e3c777283184e29533273276517a3ea7
chan <chan.jor.zz@gmail.com>
2022-12-09 21:05:29 +0900

    add db.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+// App.js PATCH
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [todo, setTodo] = useState({
+    id: Math.floor(Math.random() * 10000),
+    title: "",
+  });
+  const [todos, setTodos] = useState(null);
+
+  // patch에서 사용할 id, 수정값의 state를 추가
+  const [targetId, setTargetId] = useState(null);
+  const [editTodo, setEditTodo] = useState({
+    title: "",
+  });
+
+  const fetchTodos = async () => {
+    const { data } = await axios.get("http://localhost:3001/todos");
+    setTodos(data);
+  };
+
+  const onSubmitHandler = (todo) => {
+    axios.post("http://localhost:3001/todos", todo);
+    setTodos([...todos, todo]);
+  };
+
+  const onClickDeleteButtonHandler = (todoId) => {
+    axios.delete(`http://localhost:3001/todos/${todoId}`);
+    // setTodos([...todos, todo]); // 삭제하고 리렌더링 안됨..
+  };
+
+  // 수정버튼 이벤트 핸들러 추가 👇
+  const onClickEditButtonHandler = (todoId, edit) => {
+    axios.patch(`http://localhost:3001/todos/${todoId}`, edit);
+    // setTodos(todos); // [todos] [...todos, todo], todo 리렌더링 안됨
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmitHandler(todo);
+        }}
+      >
+        {/* 👇 수정기능에 필요한 id, 수정값 input2개와 수정하기 버튼을 추가 */}
+        <div>
+          <input
+            type="text"
+            placeholder="수정하고싶은 Todo ID"
+            onChange={(ev) => {
+              setTargetId(ev.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="수정값 입력"
+            onChange={(ev) => {
+              setEditTodo({
+                ...editTodo,
+                title: ev.target.value,
+              });
+            }}
+          />
+          <button
+            // type='button' 을 추가해야 form의 영향에서 벗어남
+            type="button"
+            onClick={() => onClickEditButtonHandler(targetId, editTodo)}
+          >
+            수정하기
+          </button>
+        </div>
+        <input
+          type="text"
+          onChange={(ev) => {
+            const { value } = ev.target;
+            setTodo({
+              ...todo,
+              title: value,
+            });
+          }}
+        />
+        <button>추가하기</button>
+      </form>
+      <div>
+        {todos?.map((todo) => (
+          <div key={todo.id}>
+            {/* todo의 아이디를 화면에 표시 */}
+            {todo.id} :{todo.title}
+            <button
+              type="button"
+              onClick={() => onClickDeleteButtonHandler(todo.id)}
+            >
+              삭제하기
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
+/* App.js GET
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // axios import 합니다.
+
+const App = () => {
+  const [todos, setTodos] = useState(null);
+  const [todo, setTodo] = useState({ title: ""})
+  // axios를 통해서 get 요청을 하는 함수를 생성합니다.
+  // 비동기처리를 해야하므로 async/await 구문을 통해서 처리합니다.
+  const fetchTodos = async () => {
+    const { data } = await axios.get("http://localhost:3001/todos");
+    setTodos(data); // 서버로부터 fetching한 데이터를 useState의 state로 set 합니다.
+  };
+
+  // 생성한 함수를 컴포넌트가 mount 됐을 떄 실행하기 위해 useEffect를 사용합니다.
+  useEffect(() => {
+    // effect 구문에 생성한 함수를 넣어 실행합니다.
+    fetchTodos();
+  }, []);
+
+  // data fetching이 정상적으로 되었는지 콘솔을 통해 확인합니다.
+  console.log(todos); // App.js:16
+  return <div>App</div>;
+};
+
+export default App;
+*/
